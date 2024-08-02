ad0ae6af836cb358f0d49a5635a71bb55575b10d
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-08-02 09:28:17 +0200

    Setup test cases in UserInterface and setup Main class to run them. Also added UML class diagrams

---
+++ src/ClassAndObject/ClassAndObjectMain.java
@@ -0,0 +1,11 @@
+package ClassAndObject;
+
+import ClassAndObject.userInterface.UserInterface;
+
+public class ClassAndObjectMain {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+
+        ui.runTests();
+    }
+}
--- src/ClassAndObject/userInterface/UserInterface.java
+++ src/ClassAndObject/userInterface/UserInterface.java
@@ -1,6 +1,8 @@
 package ClassAndObject.userInterface;
 
+import ClassAndObject.model.Color;
 import ClassAndObject.model.Line;
+import ClassAndObject.model.Style;
 
 public class UserInterface {
 
@@ -40,5 +42,59 @@ public class UserInterface {
         System.out.println(line);
         System.out.println(colorCode + output.toString() + "\u001B[0m");
 
+    }
+
+    public void runTests() {
+        testValidLine();
+        testLineWithOffset();
+        testLineWithDifferentColorAndStyle();
+        testLineWithZeroOffsetAndDifferentStyle();
+        testInvalidLineCreation();
+    }
+
+    public void testValidLine() {
+        Line line1 = new Line(3, Color.PIROS, Style.DUPLA);
+        draw(line1);
+        assert line1.getLength() == 3;
+        assert line1.getColor() == Color.PIROS;
+        assert line1.getStyle() == Style.DUPLA;
+        assert line1.getOffset() == 0;
+    }
+
+    public void testLineWithOffset() {
+        Line line2 = new Line(3, 2, Color.PIROS, Style.DUPLA);
+        draw(line2);
+        assert line2.getLength() == 3;
+        assert line2.getColor() == Color.PIROS;
+        assert line2.getStyle() == Style.DUPLA;
+        assert line2.getOffset() == 2;
+    }
+
+    public void testLineWithDifferentColorAndStyle() {
+        Line line3 = new Line(2, 3, Color.KÉK, Style.SZIMPLA);
+        draw(line3);
+        assert line3.getLength() == 2;
+        assert line3.getColor() == Color.KÉK;
+        assert line3.getStyle() == Style.SZIMPLA;
+        assert line3.getOffset() == 3;
+    }
+
+    public void testLineWithZeroOffsetAndDifferentStyle() {
+        Line line4 = new Line(5, 0, Color.ZÖLD, Style.PONT);
+        draw(line4);
+        assert line4.getLength() == 5;
+        assert line4.getColor() == Color.ZÖLD;
+        assert line4.getStyle() == Style.PONT;
+        assert line4.getOffset() == 0;
+    }
+
+    public void testInvalidLineCreation() {
+        try {
+            Line invalidLine = new Line(0, Color.PIROS, Style.DUPLA);
+            assert false : "IllegalArgumentException kivétel, ha a vonal hossza 1-nél kisebb";
+        } catch (IllegalArgumentException e) {
+            System.out.println("Egy vonal nem jött létre, mert nem létező a hossz!");
+            assert e.getMessage().equals("A vonal hossza nem lehet kisebb, mint 1");
+        }
     }
 }
